fc4e9a79221421d057e3ce109eb2e1717d93e318
Hossam AlaaElDin <hossamalaa161998@gmail.com>
2024-04-24 19:47:55 +0200

    add application controller and render module for json

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,35 @@
-class ApplicationController < ActionController::Base
+class ApplicationController < ApiController
+
+    include Render
+
+    # GET /application
+    def index
+        @applications = Application.all
+        render json: @applications
+    end
+
+    # POST /application
+    def create
+        @application = Application.new(application_params)
+        @application.save
+        render_json @application
+    end
+
+    # PATCH/PUT /application/:id
+    def update
+        @application = Application.find_by!(access_token: params[:access_token])
+        @application.update(application_params)
+        render_json @application
+    end
+
+    # GET /application/:id
+    def show
+        @application = Application.find_by!(access_token: params[:access_token])
+        render_json @application
+    end
+
+    private
+    def application_params
+        params.permit(:name)
+    end
 end
